3a2b37658db23fe028ecd7384f577ed5f2466bfe
David Vega <david@izarus.cl>
2017-05-20 04:39:57 -0400

    Revert

--- modules/izarusModalChild/actions/actions.class.php
+++ modules/izarusModalChild/actions/actions.class.php
@@ -95,10 +95,7 @@ class izarusModalChildActions extends sfActions
         //Insertar o Actualizar
         }else{
           if($form->isValid()){
-            $form->updateObject();
-            if (!$form->getValue('id')) {
-              $form->getObject()->setId(NULL);
-            }
+
             $form->getObject()->save();
 
             $componet_data = $this->getUser()->getAttribute('izarusModalChild'.$clase.$form_name);
